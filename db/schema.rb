cc3c85444429ee5818d91f26f5667a350e371ba4
Hamilton-Dias <hamiltondias1997@hotmail.com>
2019-05-10 17:01:05 -0300

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_174210) do
+ActiveRecord::Schema.define(version: 2019_05_10_194314) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "model1s", force: :cascade do |t|
     t.datetime "created_at", null: false
