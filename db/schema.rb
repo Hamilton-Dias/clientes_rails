5f2e37584454ebef6593c226ed52517ec886ed66
Hamilton-Dias <hamiltondias1997@hotmail.com>
2019-05-13 18:38:39 -0300

    Alteracoes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_10_200842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
